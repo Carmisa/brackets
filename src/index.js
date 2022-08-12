50497310993cfa2601fa69de38264ec26de4269a
Carmisa <carmisay@gmail.com>
2022-08-13 03:15:07 +0500

    feat: js code brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs = [];
+  for (item of bracketsConfig) {
+    pairs.push(item[0] + item[1]);
+  }
+  while (pairs.some(val => str.includes(val))) {
+    for (item of pairs) {
+      str = str.replace(item, '');
+    }
+  }
+  return !Boolean(str);
 }
